3c052028aade8b7d78849f2d0c35936969448774
IrishOtaku <113740004+IrishOtaku@users.noreply.github.com>
2024-01-16 20:23:42 +0000

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function sudo
+    if test "$argv[1]" = "dnf"
+        set argv[1] dnf5
+    end
+    command sudo $argv
+end
